bf6b2bba996003c4082429978d93a57d45e15e9b
yashweblife <yashweblife20@gmail.com>
2025-09-03 13:30:27 -0500

    add lib

---
+++ src/lib/index.ts
@@ -0,0 +1,20 @@
+import { Application } from "express"
+
+export function regulate(app: Application) {
+    console.log("Regulating")
+    const routes: {
+        path: string
+        method: string
+    }[] = []
+
+    app.router.stack.forEach((middleware: any) => {
+        if (middleware.route) {
+            routes.push({
+                path: middleware.route.path,
+                method: middleware.route.stack[0].method
+            })
+        }
+    })
+    console.log(routes)
+    return (routes);
+}
