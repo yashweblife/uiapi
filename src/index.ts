8d0f639f3c37edd63ad9178672275d3cb0b95cbe
yashweblife <yashweblife20@gmail.com>
2025-09-03 13:30:47 -0500

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,5 @@
-import express, { Application } from "express"
-
-export function regulate(app: Application) {
-    console.log("Regulating")
-    const routes: {
-        path: string
-        method: string
-    }[] = []
-
-    app.router.stack.forEach((middleware: any) => {
-        if (middleware.route) {
-            routes.push({
-                path: middleware.route.path,
-                method:middleware.route.stack[0].method
-            })
-        }
-    })
-    console.log(routes)
-    return (routes);
-}
-
-
+import express from "express";
+import { regulate } from "./lib/index";
 const app = express();
 app.get("/", (req, res) => {
     res.send("hello")
