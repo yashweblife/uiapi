442d6ea5566e8f277e2d16bf5f6beb32330f6980
yashweblife <yashweblife20@gmail.com>
2025-09-08 18:23:42 -0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import { regulate } from "./lib/index";
 const app = express();
-app.get("/", (req, res) => {
-    res.send("hello")
-})
-app.get("/test", () => { }, (req, res) => {
-    res.send("test")
+app.
+app.use(express.json())
+app.use(express.static("public/dist"))
+app.get("/", () => { }, (req, res) => {
+    res.send("<h1>test</h1>")
 })
 app.post("/test", () => { }, (req, res) => {
     res.send("test")
 })
 regulate(app);
+
+app.listen(3020,()=>{
+    console.log(`listening on port 3020`)
+});
