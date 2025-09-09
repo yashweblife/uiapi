22fe5e5463550b72c24d5f5f087c9047f6b39659
yashweblife <yashweblife20@gmail.com>
2025-09-09 18:08:45 -0500

    mod into function

--- src/public/src/App.tsx
+++ src/public/src/App.tsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from "react";
 function App() {
   const [routes, setRoutes] = useState<{ path: string; method: string;}[]>([]);
   useEffect(() => {
-    fetch("/dest/routes")
-      .then((res) => res.json())
-      .then((data) => {
-        setRoutes(data);
-        console.log(data);
-      });
-  })
+    getData();
+  },[])
+  const getData = async () => {
+    const res = await fetch("/dest/routes")
+    const data = await res.json()
+    setRoutes(data)
+    console.log(data); 
+  }
   return (
     <div>
       <h1>Routes</h1>
