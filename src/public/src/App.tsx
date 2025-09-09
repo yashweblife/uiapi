a2d0325e64bf4e6aaecadd02fd183d715d265457
yashweblife <yashweblife20@gmail.com>
2025-09-09 18:02:25 -0500

    add route visualizer

--- src/public/src/App.tsx
+++ src/public/src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [routes, setRoutes] = useState<{ path: string; method: string;}[]>([]);
+  useEffect(() => {
+    fetch("/dest/routes")
+      .then((res) => res.json())
+      .then((data) => {
+        setRoutes(data);
+        console.log(data);
+      });
+  })
   return (
     <div>
+      <h1>Routes</h1>
+      <ul>
+        {routes.map((route) => (
+          <li key={route.path}>
+            <h2>{route.path}</h2>
+            <p>Method: {route.method}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
