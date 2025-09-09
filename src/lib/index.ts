aaf033c81f2b00ec61e9dd981d3bec53cfb313e9
yashweblife <yashweblife20@gmail.com>
2025-09-09 17:58:51 -0500

    add modifiers

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,12 +1,13 @@
-import { Application } from "express"
-
+import express, { Application } from "express";
+import path from "path";
 export function regulate(app: Application) {
-    console.log("Regulating")
+    const __dirname = path.resolve();
+    const publicPath = path.join(__dirname, "/src/public");
+    console.log("test", publicPath)
     const routes: {
         path: string
         method: string
     }[] = []
-
     app.router.stack.forEach((middleware: any) => {
         if (middleware.route) {
             routes.push({
@@ -14,6 +15,13 @@ export function regulate(app: Application) {
                 method: middleware.route.stack[0].method
             })
         }
+    })
+    app.use(express.static("public/dist"))
+    app.get("/dest", (req, res) => {
+        res.sendFile(path.join(publicPath, "index.html"))
+    })
+    app.get("/dest/routes", (req, res) => {
+        res.send(routes)
     })
     return (routes);
 }
