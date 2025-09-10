3382100953852eccb6d30b301868ff6eb5657a7f
yashweblife <yashweblife20@gmail.com>
2025-09-10 12:05:54 -0500

    more components

--- src/public/src/components/List.tsx
+++ src/public/src/components/List.tsx
@@ -1,9 +1,33 @@
 import type { ReactNode } from "react"
 
+export function ListHeader({ title }: { title: string }) {
+    return (
+        <h1>{title}</h1>
+    )
+}
+export function ListFooter({ children }: { children: ReactNode }) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export function Item({ children }: { children: ReactNode }) {
+    return (
+        <li>
+            {children}
+        </li>
+    )
+}
+
 export default function List({ children }: { children: ReactNode }) {
     return (
-        <ul>
-            {children}
-        </ul>
+        <>
+            <ListHeader title="List Header" />
+            <ul>
+                {children}
+            </ul>
+        </>
     )
 }
