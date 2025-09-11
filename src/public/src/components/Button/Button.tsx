17b132e1f8386a3f4abbd603522fc65a46fcbefc
yashweblife <yashweblife20@gmail.com>
2025-09-11 15:24:42 -0500

    fix default style

--- src/public/src/components/Button/Button.tsx
+++ src/public/src/components/Button/Button.tsx
@@ -7,31 +7,36 @@ type ButtonProps = {
     variant?: "default" | "primary" | "secondary"
     size?: "small" | "medium" | "large"
 }
+
+
+
 export default function Button({
     click,
     children,
-    variant="default",
-    size="medium",
+    variant = "default",
+    size = "medium",
     ...props
 }: ButtonProps) {
     const [hover, setHover] = useState(false)
     const defaultStyle: CSSProperties = {
-        transition:'box-shadow 0.2s ease-in-out',
+        transition: 'box-shadow 0.2s ease-in-out',
         border: 'none',
-        boxShadow: hover?'0 0 5px rgb(100,100,100)':'0 0 5px rgb(200,200,200)',
+        boxShadow: hover ? '0 0 5px rgb(100,100,100)' : '0 0 5px rgb(200,200,200)',
         padding: '10px 20px',
         cursor: 'pointer',
         borderRadius: '5px',
+        color: 'white',
+        backgroundColor: 'rgba(32, 32, 32, 1)',
         fontSize: size === "small" ? "12px" : size === "medium" ? "16px" : "20px"
     }
     const primaryVariantStyle = {
         ...defaultStyle,
-        backgroundColor: "blue",
-        color: "white"
+        backgroundColor: "rgb(51, 226, 203)",
+        color: "rgb(32,32,32)"
     }
     const secondaryVariantStyle = {
         ...defaultStyle,
-        backgroundColor: "white",
+        backgroundColor: "rgba(51, 141, 226, 1)",
         color: "black",
     }
     return (
@@ -40,7 +45,7 @@ export default function Button({
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
             {...props}
-            style={variant === "primary" ? primaryVariantStyle : secondaryVariantStyle}
+            style={variant === "primary" ? primaryVariantStyle : variant === "secondary" ? secondaryVariantStyle : defaultStyle}
         >
             {children}
 
