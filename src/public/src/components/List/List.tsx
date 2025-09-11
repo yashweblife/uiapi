f62f2827fe63787a88c6c966ef938a49f9f9e7e1
yashweblife <yashweblife20@gmail.com>
2025-09-11 16:59:44 -0500

    fix style

--- src/public/src/components/List/List.tsx
+++ src/public/src/components/List/List.tsx
@@ -9,7 +9,7 @@ export function ListHeader({ title }: { title?: string }) {
                 borderBottom: '1px solid rgb(200,200,200)',
             }}
         >
-            <h1>{title}</h1>
+            <h2>{title}</h2>
         </header>
     )
 }
@@ -33,13 +33,20 @@ export function Item({ children }: { children?: ReactNode }) {
         <li
             style={{
                 padding: '1em',
-                display: 'flex',
-                alignItems: 'center',
                 borderBottom: '1px solid rgb(200,200,200)',
-                gap: '1em',
+                marginLeft: '1em',
             }}
         >
-            {children}
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    marginLeft: '1em',
+                    gap: '1em',
+                }}
+            >
+                {children}
+            </div>
         </li>
     )
 }
@@ -52,9 +59,9 @@ export default function List({ children, variant }: ListProps) {
         boxShadow: '0 0 5px rgb(200,200,200)',
         padding: '1em',
         borderRadius: '1em',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '1em',
+        // display: 'flex',
+        // flexDirection: 'column',
+        // gap: '1em',
     }
     if (variant === 'bullet') {
         return (
